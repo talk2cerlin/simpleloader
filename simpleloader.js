0fba52a9987143253e2f08f4676ac60a996a4063
Cerlin <cerlin@linkstreet.in>
2016-02-22 16:25:14 +0530

    Major Change
    Removed code which used loading images and using bootstrap progress bar

--- simpleloader.js
+++ simpleloader.js
@@ -1,94 +1,80 @@
 (function() {
-	var simpleloader = {
-		defaults: {
-			id: "simpleloader",
-			init: false,
-			opacity: 0.9,
-			imgurl: false,
-			imgwidth: 50,
-			imgheight: 50,
-			autoopen: false,
-			selector: false
-		},
-		init: function(obj){
-			if (typeof jQuery == 'undefined') {
-				this.log("jQuery is not found. Make sure you have jquery included");
-				return false;
-			}
-			else if(typeof $ == 'undefined'){
-				$ = jQuery;
-			}
-			$.fn.extend({
-				exists: function () {
-					return this.length !== 0;
-				}
-			});
-			if(!this.isEmpty(obj)){
-				this.updatedefaults(obj);
-			}
-			if(!$("#"+this.defaults.id).exists()){
-				var obj = (this.defaults.selector) ? this.defaults.selector : $('body');
-				obj.prepend('<div id="'+this.defaults.id+'" style="display:none;position:absolute;width:100%;top:0;background-color:black;height:100%;left:0;z-index:9999999999999999999999999;opacity:'+this.defaults.opacity+';" ></div>');
-				this.defaults.init = true;
-			}
-			if(this.defaults.imgurl)
-				$("#"+this.defaults.id).html('<img src="'+this.defaults.imgurl+'" '+((this.defaults.imgwidth)? "width="+this.defaults.imgwidth+"px" : "" )+' '+((this.defaults.imgheight)? "width="+this.defaults.imgheight+"px" : "" )+' style="position: absolute;margin: auto;top: 0;left: 0;right: 0;bottom: 0;"/>');
-			else
-				$("#"+this.defaults.id).html(' ');
-			if(this.defaults.autoopen){
-				this.fadein();
-			}
-		},
-		show: function(speed){
-			if(this.defaults.init)
-				$("#"+this.defaults.id).show(speed);
-			else
-				this.log("Please initialize the loader using simpleloader.init()");
-		},
-		hide: function(speed){
-			if(this.defaults.init)
-				$("#"+this.defaults.id).hide(speed);
-			else
-				this.log("Please initialize the loader using simpleloader.init()");
-		},
-		fadeIn: function(speed){
-			this.fadein(speed);
-		},
-		fadeOut: function(speed){
-			this.fadeout(speed);
-		},
-		fadein: function(speed){
-			if(this.defaults.init)
-				$("#"+this.defaults.id).fadeIn(speed);
-			else
-				this.log("Please initialize the loader using simpleloader.init()");
-		},
-		fadeout: function(speed){
-			if(this.defaults.init)
-				$("#"+this.defaults.id).fadeOut(speed);
-			else
-				this.log("Please initialize the loader using simpleloader.init()");
-		},
-		remove: function(){
-			$("#"+this.defaults.id).remove();
-			this.defaults.init = false;
-		},
-		log: function(msg){
-			if(window.console)
-				console.log(msg);
-		},
-		updatedefaults: function(obj){
-			for (var attrname in obj) { 
-				this.defaults[attrname] = obj[attrname]; 
-			}
-		},
-		isempty: function(obj) {
-			for(var prop in obj) {
-				if(obj.hasOwnProperty(prop))
-					return false;
-			}
-			return true;
-		}
-	};
-	window.simpleloader = simpleloader;
-})()
+    var e = {
+        initFlag: false,
+        defaults: {
+            id: "simpleloader",
+            opacity: .5,
+            autoOpen: false,
+        },
+        init: function(e) {
+            if (typeof jQuery == "undefined") {
+                this.log("jQuery is not found. Make sure you have jquery included");
+                return false
+            } else if (typeof $ == "undefined") {
+                $ = jQuery
+            }
+            $.fn.extend({
+                exists: function() {
+                    return this.length !== 0
+                }
+            });
+            if (!this.isEmpty(e)) {
+                this.updatedefaults(e)
+            }
+            if (!$("." + this.defaults.id).exists()) {
+                var $this = this
+                $(document).ready(function(){
+                    var e = $("body");
+                    e.append('<div class="' + $this.defaults.id + '" style="display:none;position:fixed;width:100%;top:0;background-color:black;height:100%;left:0;z-index:99999;opacity:' + $this.defaults.opacity + ';" ></div>');
+                    e.append('<div class="' + $this.defaults.id + ' progress progress-striped active" style="display:none; position: fixed !important; width: 16%; z-index: 100000; height: 20px;margin: auto; left: 0; top: 0; right: 0; bottom: 0; "> <div style="width: 100%;" class="progress-bar progress-bar-success">Loading...</div> </div>');
+                });
+                this.initFlag = true;
+            }
+            if (this.defaults.autoOpen) {
+                this.fadeIn()
+            }
+        },
+        show: function(e) {
+            if (this.initFlag) $("." + this.defaults.id).show(e);
+            else this.log("Please initialize the loader using simpleloader.init()")
+        },
+        hide: function(e) {
+            if (this.initFlag) $("." + this.defaults.id).hide(e);
+            else this.log("Please initialize the loader using simpleloader.init()")
+        },
+        fadeIn: function(e) {
+            this.fadein(e)
+        },
+        fadeOut: function(e) {
+            this.fadeout(e)
+        },
+        fadein: function(e) {
+            if (this.initFlag) $("." + this.defaults.id).fadeIn(e);
+            else this.log("Please initialize the loader using simpleloader.init()")
+        },
+        fadeout: function(e) {
+            if (this.initFlag) $("." + this.defaults.id).fadeOut(e);
+            else this.log("Please initialize the loader using simpleloader.init()")
+        },
+        remove: function() {
+            $("." + this.defaults.id).remove();
+            this.initFlag = false
+        },
+        log: function(e) {
+            if (window.console) console.log(e)
+        },
+        updatedefaults: function(e) {
+            for (var t in e) {
+                this.defaults[t] = e[t]
+            }
+        },
+        isEmpty: function(e) {
+            for (var t in e) {
+                if (e.hasOwnProperty(t)) return false
+            }
+            return true
+        }
+    };
+    window.simpleloader = e;
+    return e;
+})().init();
